11280a95678100242da48701558718a452a6b4ee
steelbridge-io <chris@steelbridge.io>
2021-01-23 17:38:53 -0800

    Added hero video url input

--- sbm-image-field/inc/sanitize_streamreport_image.php
+++ sbm-image-field/inc/sanitize_streamreport_image.php
@@ -14,50 +14,54 @@ function streamreport_meta_save( $post_id ) {
         return;
     }
  	
-	// Checks for input and saves if needed 
+	// Checks for input and saves if needed
 		if( isset( $_POST[ 'featured1-image' ] ) ) {
 				update_post_meta( $post_id, 'featured1-image', $_POST[ 'featured1-image' ] );
 		}
 	
-	// Checks for input and saves if needed 
+	// Checks for input and saves if needed
 		if( isset( $_POST[ 'featured2-image' ] ) ) {
 				update_post_meta( $post_id, 'featured2-image', $_POST[ 'featured2-image' ] );
 		}
 	
-	// Checks for input and saves if needed 
+	// Checks for input and saves if needed
 		if( isset( $_POST[ 'featured3-image' ] ) ) {
 				update_post_meta( $post_id, 'featured3-image', $_POST[ 'featured3-image' ] );
 		}
 	
-	// Checks for input and saves if needed 
+	// Checks for input and saves if needed
 		if( isset( $_POST[ 'featured4-image' ] ) ) {
 				update_post_meta( $post_id, 'featured4-image', $_POST[ 'featured4-image' ] );
 		}
 	
-	// Checks for input and saves if needed 
+	// Checks for input and saves if needed
 		if( isset( $_POST[ 'rivers-image' ] ) ) {
 				update_post_meta( $post_id, 'rivers-image', $_POST[ 'rivers-image' ] );
 		}
 	
-	// Checks for input and saves if needed 
+	// Checks for input and saves if needed
 		if( isset( $_POST[ 'lakes-image' ] ) ) {
 				update_post_meta( $post_id, 'lakes-image', $_POST[ 'lakes-image' ] );
 		}
 	
-	// Checks for input and saves if needed 
+	// Checks for input and saves if needed
 		if( isset( $_POST[ 'private-waters-image' ] ) ) {
 				update_post_meta( $post_id, 'private-waters-image', $_POST[ 'private-waters-image' ] );
 		}
 	
-	// Checks for input and saves if needed 
+	// Checks for input and saves if needed
 		if( isset( $_POST[ 'tfs-logo-report' ] ) ) {
 				update_post_meta( $post_id, 'tfs-logo-report', $_POST[ 'tfs-logo-report' ] );
 		}
 	
-	// Checks for input and saves if needed 
+	// Checks for input and saves if needed
 		if( isset( $_POST[ 'report-image' ] ) ) {
 				update_post_meta( $post_id, 'report-image', $_POST[ 'report-image' ] );
 		}
+		
+		if( isset( $POST[ 'report-video' ] ) ) {
+		    update_post_meta( $post_id, 'report-video', $_POST[ 'report-video' ] );
+    }
  
 }
 add_action( 'save_post', 'streamreport_meta_save' );
