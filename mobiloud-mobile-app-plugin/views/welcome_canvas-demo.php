aa87c9ee121aa60d0ae709b35a8eea0bf5b3621e
steelbridge-io <chris@steelbridge.io>
2023-08-23 14:08:27 -0700

    Updates

---
+++ mobiloud-mobile-app-plugin/views/welcome_canvas-demo.php
@@ -0,0 +1,9 @@
+<div class="ml2-block ml2-welcome-block welcome-step-2 canvas-setup">
+	<div class="ml2-body text-left">
+    <h3 class="title_big"><?php esc_html_e( 'Our Canvas platform would be a better fit for your app! Please schedule a call with our team to discuss more details and next steps' ); ?></h3>
+    <div class="ml-col-row ml-init-button canvas-setup-wrapper">
+        <button type="submit" name="submit" id="submit" class="button button-hero button-primary ladda-button" data-style="zoom-out"><span class="ladda-label"><?php esc_html_e( 'I want to book a time!' ); ?></span><span class="ladda-spinner"></span></button>
+    </div>
+    <a href="<?php echo esc_url( admin_url( 'admin.php?page=mobiloud&step=design&page_number=3' ) ) ?>" class="skip-canvas-setup-link"><?php esc_html_e( 'No, take me to the configuration', 'mobiloud' ); ?></a>
+    </div>
+</div>
