8e6b989dfb7e01a09816bc5ed9746cc18d5453b9
steelbridge-io <chris@steelbridge.io>
2024-07-26 00:24:33 -0700

    Add media uploader functionality for multiple image buttons
    This commit enhances the JavaScript functionality by adding media uploader options for various elements such as the landing page logo, hero image, and multiple grid items. This allows for easier selection and preview of images directly within the WordPress admin panel.

--- tfs-parent-theme-options/js/wp-image-api.js
+++ tfs-parent-theme-options/js/wp-image-api.js
@@ -1,16 +1,137 @@
-jQuery(document).ready( function($){ "use strict";
+
+    jQuery(function($){
+    $('#landing-page-logo-button').on('click', function () {
+        var _this = $(this);
+        var frame = wp.media({
+            title: 'Select or Upload an Image',
+            button: {
+                text: 'Use this image',
+            },
+            multiple: false
+        });
+
+        frame.on('select', function () {
+            var attachment = frame.state().get('selection').first().toJSON();
+            _this.prev('#landing-page-logo').val(attachment.url);
+            $('#landing-page-logo-preview').attr('src', attachment.url);
+        });
+
+        frame.open();
+    });
+
+        $('#landing-page-image-button').on('click', function () {
+            var _this = $(this);
+            var frame = wp.media({
+                title: 'Select or Upload an Image',
+                button: {
+                    text: 'Use this image',
+                },
+                multiple: false
+            });
+
+            frame.on('select', function () {
+                var attachment = frame.state().get('selection').first().toJSON();
+                _this.prev('#landing-page-image').val(attachment.url);
+                $('#landing-page-image-preview').attr('src', attachment.url);
+            });
+
+            frame.open();
+        });
+
+        $('#grid-item-1-img-button').on('click', function () {
+            var _this = $(this);
+            var frame = wp.media({
+                title: 'Select or Upload an Image',
+                button: {
+                    text: 'Use this image',
+                },
+                multiple: false
+            });
+
+            frame.on('select', function () {
+                var attachment = frame.state().get('selection').first().toJSON();
+                _this.prev('#grid-item-1-img').val(attachment.url);
+                $('#grid-item-1-img-preview').attr('src', attachment.url);
+            });
+
+            frame.open();
+        });
+
+        $('#grid-item-2-img-button').on('click', function () {
+            var _this = $(this);
+            var frame = wp.media({
+                title: 'Select or Upload an Image',
+                button: {
+                    text: 'Use this image',
+                },
+                multiple: false
+            });
+
+            frame.on('select', function () {
+                var attachment = frame.state().get('selection').first().toJSON();
+                _this.prev('#grid-item-2-img').val(attachment.url);
+                $('#grid-item-2-img-preview').attr('src', attachment.url);
+            });
+
+            frame.open();
+        });
+
+        $('#grid-item-3-img-button').on('click', function () {
+            var _this = $(this);
+            var frame = wp.media({
+                title: 'Select or Upload an Image',
+                button: {
+                    text: 'Use this image',
+                },
+                multiple: false
+            });
+
+            frame.on('select', function () {
+                var attachment = frame.state().get('selection').first().toJSON();
+                _this.prev('#grid-item-3-img').val(attachment.url);
+                $('#grid-item-3-img-preview').attr('src', attachment.url);
+            });
+
+            frame.open();
+        });
+
+        $('#grid-item-4-img-button').on('click', function () {
+            var _this = $(this);
+            var frame = wp.media({
+                title: 'Select or Upload an Image',
+                button: {
+                    text: 'Use this image',
+                },
+                multiple: false
+            });
+
+            frame.on('select', function () {
+                var attachment = frame.state().get('selection').first().toJSON();
+                _this.prev('#grid-item-4-img').val(attachment.url);
+                $('#grid-item-4-img-preview').attr('src', attachment.url);
+            });
+
+            frame.open();
+        });
+
+    });
+
+
+
+/*jQuery(document).ready(function ($) {
+    "use strict";
 
     // Signature Travel Template.
     var landing_pg_logo_frame;
 
     // Runs when the image button is clicked.
-    $('#landing-page-logo-button').click(function(e){
+    $('#landing-page-logo-button').click(function (e) {
 
         // Prevents the default action from occuring.
         e.preventDefault();
 
         // If the frame already exists, re-open it.
-        if ( landing_pg_logo_frame ) {
+        if (landing_pg_logo_frame) {
             landing_pg_logo_frame.open();
             return;
         }
@@ -18,12 +139,12 @@ jQuery(document).ready( function($){ "use strict";
         // Sets up the media library frame
         landing_pg_logo_frame = wp.media.frames.landing_pg_logo_frame = wp.media({
             title: meta_image.title,
-            button: { text:  meta_image.button },
-            library: { type: 'image' }
+            button: {text: meta_image.button},
+            library: {type: 'image'}
         });
 
         // Runs when an image is selected.
-        landing_pg_logo_frame.on('select', function(){
+        landing_pg_logo_frame.on('select', function () {
 
             // Grabs the attachment selection and creates a JSON representation of the model.
             var media_attachment = landing_pg_logo_frame.state().get('selection').first().toJSON();
@@ -41,13 +162,13 @@ jQuery(document).ready( function($){ "use strict";
     var landing_pg_hero_frame;
 
     // Runs when the image button is clicked.
-    $('#landing-page-image-button').click(function(e){
+    $('#landing-page-image-button').click(function (e) {
 
         // Prevents the default action from occuring.
         e.preventDefault();
 
         // If the frame already exists, re-open it.
-        if ( landing_pg_hero_frame ) {
+        if (landing_pg_hero_frame) {
             landing_pg_hero_frame.open();
             return;
         }
@@ -55,12 +176,12 @@ jQuery(document).ready( function($){ "use strict";
         // Sets up the media library frame
         landing_pg_hero_frame = wp.media.frames.landing_pg_hero_frame = wp.media({
             title: meta_image.title,
-            button: { text:  meta_image.button },
-            library: { type: 'image' }
+            button: {text: meta_image.button},
+            library: {type: 'image'}
         });
 
         // Runs when an image is selected.
-        landing_pg_hero_frame.on('select', function(){
+        landing_pg_hero_frame.on('select', function () {
 
             // Grabs the attachment selection and creates a JSON representation of the model.
             var media_attachment = landing_pg_hero_frame.state().get('selection').first().toJSON();
@@ -72,4 +193,4 @@ jQuery(document).ready( function($){ "use strict";
         // Opens the media library frame.
         landing_pg_hero_frame.open();
     });
-});
+});*/
