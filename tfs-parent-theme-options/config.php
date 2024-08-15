a331e39278559407b4e35fd69bfa4b5a3345afaf
steelbridge-io <chris@steelbridge.io>
2024-08-14 23:05:46 -0700

    Add allowed HTML tags configuration file
    Includes an array defining permissible HTML tags and attributes for
    the 'tfs-parent-theme-options' plugin. Provides a foundational setup
    for sanitizing and ensuring safe HTML outputs within the plugin.

---
+++ tfs-parent-theme-options/config.php
@@ -0,0 +1,27 @@
+<?php
+   $allowed_tags = array(
+       'a' => array(
+           'href' => array(),
+           'title' => array(),
+       ),
+       'br' => array(),
+       'em' => array(),
+       'strong' => array(),
+       'p' => array(),
+       'ul' => array(),
+       'li' => array(),
+       'ol' => array(),
+       'img' => array(
+           'src' => array(),
+           'alt' => array(),
+           'title' => array(),
+           'width' => array(),
+           'height' => array(),
+       ),
+       'h1' => array (),
+       'h2' => array (),
+       'h3' => array (),
+       'h4' => array (),
+       'span' => array(),
+       // Add more tags and attributes as needed
+   );
