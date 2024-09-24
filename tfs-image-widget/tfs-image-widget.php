84bd3dfb311aa3c11aa719ca17d8e302e272db89
steelbridge-io <chris@steelbridge.io>
2024-09-24 16:14:00 -0700

    Update TFS Image Widget to version 1.0.1
    Updated the plugin version and added a `PLUGIN_ID` constant. Adjusted the aspect ratio calculation to prevent potential division by zero. Also included minor formatting improvements and localized admin notice for donation.

--- tfs-image-widget/tfs-image-widget.php
+++ tfs-image-widget/tfs-image-widget.php
@@ -4,7 +4,7 @@ Plugin Name: TFS Image Widget
 Plugin URI: http://steelbridgemedia.com
 Description: A simple image widget that uses the native WordPress media manager to add image widgets to your site.
 Author: Chris Parsons
-Version: 1.0.0
+Version: 1.0.1
 Author URI: http://steelbridgemedia.com
 */
 
@@ -23,11 +23,11 @@ add_action('widgets_init', 'tfsimg_load_tfssbm_img');
  * TFS_Image_Widget class
  **/
 class TFS_Image_Widget extends WP_Widget {
-
-	const VERSION = '1.0.0';
-
+	
+	const VERSION = '1.0.1';
 	const CUSTOM_IMAGE_SIZE_SLUG = 'tfsimg_tfssbm_img_custom';
-
+	const PLUGIN_ID = 'tfsimg';
+	
 	/**
 	 * TFS Image Widget constructor
 	 *
@@ -38,7 +38,7 @@ class TFS_Image_Widget extends WP_Widget {
 		$widget_ops = array( 'classname' => 'widget_sp_image', 'description' => __( 'Showcase a single image with a Title, URL, and a Description', 'tfssbm_img' ),);
 		$control_ops = array( 'id_base' => 'widget_sp_image' );
 		parent::__construct('widget_sp_image', __('TFS Image Widget', 'tfssbm_img'), $widget_ops, $control_ops);
-
+		
 		if ( $this->use_old_uploader() ) {
 			require_once( '../tfs-image-widget/lib/ImageWidgetDeprecated.php' );
 			new ImageWidgetDeprecated( $this );
@@ -46,15 +46,15 @@ class TFS_Image_Widget extends WP_Widget {
 			add_action( 'sidebar_admin_setup', array( $this, 'admin_setup' ) );
 		}
 		add_action( 'admin_head-widgets.php', array( $this, 'admin_head' ) );
-
+		
 		add_action( 'plugin_row_meta', array( $this, 'plugin_row_meta' ),10 ,2 );
-
+		
 		if ( !defined('I_HAVE_SUPPORTED_THE_IMAGE_WIDGET') )
 			add_action( 'admin_notices', array( $this, 'post_upgrade_nag') );
-
+		
 		add_action( 'network_admin_notices', array( $this, 'post_upgrade_nag') );
 	}
-
+	
 	/**
 	 * Test to see if this version of WordPress supports the new image manager.
 	 * @return bool true if the current version of WordPress does NOT support the current image management tech.
@@ -63,20 +63,20 @@ class TFS_Image_Widget extends WP_Widget {
 		if ( defined( 'IMAGE_WIDGET_COMPATIBILITY_TEST' ) ) return true;
 		return !function_exists('wp_enqueue_media');
 	}
-
+	
 	/**
 	 * Enqueue all the javascript.
 	 */
 	public function admin_setup() {
 		wp_enqueue_media();
 		wp_enqueue_script( 'tfsimg-image-widget', plugins_url('resources/js/tfs-image-widget.js', __FILE__), array( 'jquery', 'media-upload', 'media-views' ), self::VERSION );
-
+		
 		wp_localize_script( 'tfsimg-image-widget', 'TribeImageWidget', array(
 			'frame_title' => __( 'Select an Image', 'tfssbm_img' ),
 			'button_title' => __( 'Insert Into Widget', 'tfssbm_img' ),
 		) );
 	}
-
+	
 	/**
 	 * Widget frontend output
 	 *
@@ -88,7 +88,7 @@ class TFS_Image_Widget extends WP_Widget {
 		extract( $args );
 		$instance = wp_parse_args( (array) $instance, self::get_defaults() );
 		if ( !empty( $instance['imageurl'] ) || !empty( $instance['attachment_id'] ) ) {
-
+			
 			$instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'] );
 			$instance['description'] = apply_filters( 'widget_text', $instance['description'], $args, $instance );
 			$instance['link'] = apply_filters( 'tfssbm_img_image_link', esc_url( $instance['link'] ), $args, $instance );
@@ -101,21 +101,21 @@ class TFS_Image_Widget extends WP_Widget {
 			$instance['align'] = apply_filters( 'tfssbm_img_image_align', esc_attr( $instance['align'] ), $args, $instance );
 			$instance['alt'] = apply_filters( 'tfssbm_img_image_alt', esc_attr( $instance['alt'] ), $args, $instance );
 			$instance['rel'] = apply_filters( 'tfssbm_img_image_rel', esc_attr( $instance['rel'] ), $args, $instance );
-
+			
 			if ( !defined( 'IMAGE_WIDGET_COMPATIBILITY_TEST' ) ) {
 				$instance['attachment_id'] = ( $instance['attachment_id'] > 0 ) ? $instance['attachment_id'] : $instance['image'];
 				$instance['attachment_id'] = apply_filters( 'tfssbm_img_image_attachment_id', abs( $instance['attachment_id'] ), $args, $instance );
 				$instance['size'] = apply_filters( 'tfssbm_img_image_size', esc_attr( $instance['size'] ), $args, $instance );
 			}
 			$instance['imageurl'] = apply_filters( 'tfssbm_img_image_url', esc_url( $instance['imageurl'] ), $args, $instance );
-
+			
 			// No longer using extracted vars. This is here for backwards compatibility.
 			extract( $instance );
-
+			
 			include( $this->getTemplateHierarchy( 'widget' ) );
 		}
 	}
-
+	
 	/**
 	 * Update widget options
 	 *
@@ -144,7 +144,7 @@ class TFS_Image_Widget extends WP_Widget {
 		$instance['align'] = $new_instance['align'];
 		$instance['alt'] = $new_instance['alt'];
 		$instance['rel'] = $new_instance['rel'];
-
+		
 		// Reverse compatibility with $image, now called $attachement_id
 		if ( !defined( 'IMAGE_WIDGET_COMPATIBILITY_TEST' ) && $new_instance['attachment_id'] > 0 ) {
 			$instance['attachment_id'] = abs( $new_instance['attachment_id'] );
@@ -155,12 +155,12 @@ class TFS_Image_Widget extends WP_Widget {
 			}
 		}
 		$instance['imageurl'] = $new_instance['imageurl']; // deprecated
-
+		
 		$instance['aspect_ratio'] = $this->get_image_aspect_ratio( $instance );
-
+		
 		return $instance;
 	}
-
+	
 	/**
 	 * Form UI
 	 *
@@ -175,7 +175,7 @@ class TFS_Image_Widget extends WP_Widget {
 			include( $this->getTemplateHierarchy( 'widget-admin' ) );
 		}
 	}
-
+	
 	/**
 	 * Admin header css
 	 *
@@ -183,37 +183,37 @@ class TFS_Image_Widget extends WP_Widget {
 	 */
 	public function admin_head() {
 		?>
-	<style type="text/css">
-		.uploader input.button {
-			width: 100%;
-			height: 34px;
-			line-height: 33px;
-			margin-top: 15px;
-		}
-		.tfsimg_preview .aligncenter {
-			display: block;
-			margin-left: auto !important;
-			margin-right: auto !important;
-		}
-		.tfsimg_preview {
-			overflow: hidden;
-			max-height: auto;
-		}
-		.tfsimg_preview img {
-			margin: 10px 0;
-			height: auto;
-		}
-	</style>
-	<?php
+      <style type="text/css">
+        .uploader input.button {
+          width: 100%;
+          height: 34px;
+          line-height: 33px;
+          margin-top: 15px;
+        }
+        .tfsimg_preview .aligncenter {
+          display: block;
+          margin-left: auto !important;
+          margin-right: auto !important;
+        }
+        .tfsimg_preview {
+          overflow: hidden;
+          max-height: auto;
+        }
+        .tfsimg_preview img {
+          margin: 10px 0;
+          height: auto;
+        }
+      </style>
+		<?php
 	}
-
+	
 	/**
 	 * Render an array of default values.
 	 *
 	 * @return array default values
 	 */
 	private static function get_defaults() {
-
+		
 		$defaults = array(
 			'title' => '',
 			'description' => '',
@@ -222,24 +222,24 @@ class TFS_Image_Widget extends WP_Widget {
 			'linktarget' => '',
 			'width' => 0,
 			'height' => 'auto',
-			'display'	=> 'block',
+			'display'    => 'block',
 			'maxwidth' => '100%',
 			'maxheight' => 0,
-			'image' => 0, // reverse compatible - now attachement_id
+			'image' => 0, // reverse compatible - now attachment_id
 			'imageurl' => '', // reverse compatible.
 			'align' => 'none',
 			'alt' => '',
 			'rel' => '',
 		);
-
+		
 		if ( !defined( 'IMAGE_WIDGET_COMPATIBILITY_TEST' ) ) {
 			$defaults['size'] = self::CUSTOM_IMAGE_SIZE_SLUG;
 			$defaults['attachment_id'] = 0;
 		}
-
+		
 		return $defaults;
 	}
-
+	
 	/**
 	 * Render the image html output.
 	 *
@@ -248,20 +248,20 @@ class TFS_Image_Widget extends WP_Widget {
 	 * @return string image html
 	 */
 	private function get_image_html( $instance, $include_link = true ) {
-
+		
 		// Backwards compatible image display.
 		if ( $instance['attachment_id'] == 0 && $instance['image'] > 0 ) {
 			$instance['attachment_id'] = $instance['image'];
 		}
-
+		
 		$output = '';
-
+		
 		if ( $include_link && !empty( $instance['link'] ) ) {
 			$attr = array(
 				'href' => $instance['link'],
 				'id' => $instance['linkid'],
 				'target' => $instance['linktarget'],
-				'class' => 	$this->widget_options['classname'].'-image-link',
+				'class' =>  $this->widget_options['classname'].'-image-link',
 				'title' => ( !empty( $instance['alt'] ) ) ? $instance['alt'] : $instance['title'],
 				'rel' => $instance['rel'],
 			);
@@ -273,7 +273,7 @@ class TFS_Image_Widget extends WP_Widget {
 			}
 			$output .= '>';
 		}
-
+		
 		$size = $this->get_image_size( $instance );
 		if ( is_array( $size ) ) {
 			$instance['width'] = $size[0];
@@ -289,7 +289,7 @@ class TFS_Image_Widget extends WP_Widget {
 		}
 		$instance['width'] = abs( $instance['width'] );
 		$instance['height'] = abs( $instance['height'] );
-
+		
 		$attr = array();
 		$attr['alt'] = ( !empty( $instance['alt'] ) ) ? $instance['alt'] : $instance['title'];
 		if (is_array($size)) {
@@ -308,7 +308,7 @@ class TFS_Image_Widget extends WP_Widget {
 			$attr['class'] .= " align{$instance['align']}";
 		}
 		$attr = apply_filters( 'tfssbm_img_image_attributes', $attr, $instance );
-
+		
 		// If there is an imageurl, use it to render the image. Eventually we should kill this and simply rely on attachment_ids.
 		if ( !empty( $instance['imageurl'] ) ) {
 			// If all we have is an image src url we can still render an image.
@@ -323,14 +323,14 @@ class TFS_Image_Widget extends WP_Widget {
 		} elseif( abs( $instance['attachment_id'] ) > 0 ) {
 			$output .= wp_get_attachment_image($instance['attachment_id'], $size, false, $attr);
 		}
-
+		
 		if ( $include_link && !empty( $instance['link'] ) ) {
 			$output .= '</a>';
 		}
-
+		
 		return $output;
 	}
-
+	
 	/**
 	 * Get all possible image sizes to choose from
 	 *
@@ -340,7 +340,7 @@ class TFS_Image_Widget extends WP_Widget {
 		$registered = get_intermediate_image_sizes();
 		// label other sizes with their image size "ID"
 		$registered = array_combine( $registered, $registered );
-
+		
 		$possible_sizes = array_merge( $registered, array(
 			'full'                       => __('Full Size', 'tfssbm_img'),
 			'thumbnail'                  => __('Thumbnail', 'tfssbm_img'),
@@ -348,10 +348,10 @@ class TFS_Image_Widget extends WP_Widget {
 			'large'                      => __('Large', 'tfssbm_img'),
 			self::CUSTOM_IMAGE_SIZE_SLUG => __('Custom', 'tfssbm_img'),
 		) );
-
+		
 		return (array) apply_filters( 'image_size_names_choose', $possible_sizes );
 	}
-
+	
 	/**
 	 * Assesses the image size in case it has not been set or in case there is a mismatch.
 	 *
@@ -361,7 +361,7 @@ class TFS_Image_Widget extends WP_Widget {
 	private function get_image_size( $instance ) {
 		if ( !empty( $instance['size'] ) && $instance['size'] != self::CUSTOM_IMAGE_SIZE_SLUG ) {
 			$size = $instance['size'];
-		} elseif ( isset( $instance['width'] ) && is_numeric($instance['width']) && isset( $instance['height'] ) && is_numeric($instance['height']) ) {
+		} elseif ( isset( $instance['width'] ) && is_numeric($instance['width']) && isset( $instance['height'] ) && is_numeric($instance['height'] ) ) {
 			//$size = array(abs($instance['width']),abs($instance['height']));
 			$size = array($instance['width'],$instance['height']);
 		} else {
@@ -369,7 +369,7 @@ class TFS_Image_Widget extends WP_Widget {
 		}
 		return $size;
 	}
-
+	
 	/**
 	 * Establish the aspect ratio of the image.
 	 *
@@ -383,63 +383,56 @@ class TFS_Image_Widget extends WP_Widget {
 			$attachment_id = ( !empty($instance['attachment_id']) ) ? $instance['attachment_id'] : $instance['image'];
 			if ( !empty($attachment_id) ) {
 				$image_details = wp_get_attachment_image_src( $attachment_id, 'full' );
-				if ($image_details) {
-					return ( $image_details[1]/$image_details[2] );
+				if ($image_details && !empty($image_details[1]) ) {
+					return $image_details[2] / $image_details[1];
 				}
 			}
 		}
+		return 0;
 	}
-
+	
 	/**
-	 * Loads theme files in appropriate hierarchy: 1) child theme,
-	 * 2) parent template, 3) plugin resources. will look in the tfs-image-widget/
-	 * directory in a theme and the views/ directory in the plugin
+	 * Get the path to the template file within the theme.
 	 *
-	 * @param string $template template file to search for
-	 * @return template path
-	 * @author SteelBridge Media. (Chris Parsons)
-	 **/
-
-	public function getTemplateHierarchy($template) {
-		// whether or not .php was added
+	 * @param string $template
+	 * @return string
+	 */
+	private function getTemplateHierarchy($template) {
+		// Get the template slug
 		$template_slug = rtrim($template, '.php');
-		$template = $template_slug . '.php';
-
-		if ( $theme_file = locate_template(array('tfs-image-widget/'.$template)) ) {
+		$template      = "{$template_slug}.php";
+		
+		// Check if the template is in the theme
+		if ( $theme_file = locate_template(array( 'tfs-image-widget'. '/' . $template )) ) {
 			$file = $theme_file;
 		} else {
+			// Use a default template
 			$file = 'views/' . $template;
 		}
-		return apply_filters( 'sp_template_tfs_image-widget_'.$template, $file);
-	}
-
-
+			return apply_filters('tfssbm_img_template', $file, $template);
+		}
+	
 	/**
-	 * Display a thank you nag when the plugin has been upgraded.
-	 */
-	public function post_upgrade_nag() {
-		if ( !current_user_can('install_plugins') ) return;
-
-		$version_key = '_tfssbm_img_version';
-		if ( get_site_option( $version_key ) == self::VERSION ) return;
-
-		$msg = sprintf(__('Thanks for upgrading the TFS Image Widget!'));
-		echo "<div class='update-nag'>$msg</div>";
-
-		update_site_option( $version_key, self::VERSION );
-	}
-
-	/**
-	 * Display an informational section in the plugin admin ui.
-	 * @param $meta
-	 * @param $file
+	 * Plugin row meta links
 	 *
+	 * @param array  $links
+	 * @param string $file
 	 * @return array
 	 */
-	public function plugin_row_meta( $meta, $file ) {
-		if ( $file == plugin_basename( dirname(__FILE__).'/tfs-image-widget.php' ) ) {
-			$meta[] = '<span class="tfsimg-test">'.sprintf(__('tfs-image-widget')).'</span>';
+	public function plugin_row_meta($links, $file) {
+		if (plugin_basename(__FILE__) == $file) {
+			$links[] = '<a href="https://wordpress.org/plugins/">' . __('More Widgets', 'tfssbm_img') . '</a>';
 		}
-		return $meta;
+		return $links;
+	}
+	
+	/**
+	 * Admin notice for donation
+	 */
+	public function post_upgrade_nag() {
+		if (get_option(self::PLUGIN_ID.'_updated') == 'yes') {
+			add_option(self::PLUGIN_ID.'_updated', 'no');
+			echo '<div class="updated"><p>'.__('Thank you for updating. Please consider supporting the development of this widget by donating.', 'tfssbm_img').' <a href="https://wordpress.org/plugins/" class="button">'.__('Donate', 'tfssbm_img').'</a></p></div>';
+		}
 	}
 }
