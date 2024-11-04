8c6d39955ee1268074e6bbf2231c3bac3f840229
steelbridge-io <chris@steelbridge.io>
2024-11-04 00:30:18 -0800

    Add sanitization for guest data metadata fields
    Introduced a new sanitize-meta.php file that processes and sanitizes various metadata fields from guest data submissions. This ensures that all input data is safely sanitized using sanitize_text_field before being stored in post meta.

---
+++ guest-data-app/sanitize/sanitize-meta.php
@@ -0,0 +1,158 @@
+<?php
+
+
+  if (isset($_POST['gda_meta_field'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field']);
+    update_post_meta($post_id, '_gda_meta_key', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_table_header_title'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_table_header_title']);
+    update_post_meta($post_id, '_gda_meta_key_table_header_title', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_first_name_id'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_first_name_id']);
+    update_post_meta($post_id, '_gda_meta_key_first_name_id', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_last_name_id'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_last_name_id']);
+    update_post_meta($post_id, '_gda_meta_key_last_name_id', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_header_title_res'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_header_title_res']);
+    update_post_meta($post_id, '_gda_meta_key_header_title_res', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_reservation_id'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_reservation_id']);
+    update_post_meta($post_id, '_gda_meta_key_reservation_id', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_header_date_of_arrival'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_header_date_of_arrival']);
+    update_post_meta($post_id, '_gda_meta_key_header_date_of_arrival', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_date_of_arrival_id'])) {
+    $gda_arrival_data = sanitize_text_field($_POST['gda_meta_field_date_of_arrival_id']);
+    update_post_meta($post_id, '_gda_meta_key_date_of_arrival_id', $gda_arrival_data);
+  }
+
+  if (isset($_POST['gda_meta_field_header_date_of_departure'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_header_date_of_departure']);
+    update_post_meta($post_id, '_gda_meta_key_header_date_of_departure', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_date_of_departure_id'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_date_of_departure_id']);
+    update_post_meta($post_id, '_gda_meta_key_date_of_departure_id', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_header_cell_phone'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_header_cell_phone']);
+    update_post_meta($post_id, '_gda_meta_key_header_cell_phone', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_cell_phone_id'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_cell_phone_id']);
+    update_post_meta($post_id, '_gda_meta_key_cell_phone_id', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_header_date_of_birth'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_header_date_of_birth']);
+    update_post_meta($post_id, '_gda_meta_key_header_date_of_birth', $gda_data);
+  }
+
+  if (isset($_POST['gda_meta_field_date_of_birth_id'])) {
+    $gda_data = sanitize_text_field($_POST['gda_meta_field_date_of_birth_id']);
+    update_post_meta($post_id, '_gda_meta_key_date_of_birth_id', $gda_data);
+  }
+
+if (isset($_POST['gda_meta_field_header_body_weight'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_header_body_weight']);
+  update_post_meta($post_id, '_gda_meta_key_header_body_weight', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_body_weight_id'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_body_weight_id']);
+  update_post_meta($post_id, '_gda_meta_key_body_weight_id', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_header_emergency_contact'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_header_emergency_contact']);
+  update_post_meta($post_id, '_gda_meta_key_header_emergency_contact', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_ec_first_name_id'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_ec_first_name_id']);
+  update_post_meta($post_id, '_gda_meta_key_ec_first_name_id', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_ec_last_name_id'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_ec_last_name_id']);
+  update_post_meta($post_id, '_gda_meta_key_ec_last_name_id', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_header_relationship_to_traveler'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_header_relationship_to_traveler']);
+  update_post_meta($post_id, '_gda_meta_key_header_relationship_to_traveler', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_relationship_to_traveler_id'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_relationship_to_traveler_id']);
+  update_post_meta($post_id, '_gda_meta_key_relationship_to_traveler_id', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_header_ec_tel_number'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_header_ec_tel_number']);
+  update_post_meta($post_id, '_gda_meta_key_header_ec_tel_number', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_ec_tel_number_id'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_ec_tel_number_id']);
+  update_post_meta($post_id, '_gda_meta_key_ec_tel_number_id', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_header_purchase_cancellation_ins'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_header_purchase_cancellation_ins']);
+  update_post_meta($post_id, '_gda_meta_key_header_purchase_cancellation_ins', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_purchase_cancellation_ins_id'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_purchase_cancellation_ins_id']);
+  update_post_meta($post_id, '_gda_meta_key_purchase_cancellation_ins_id', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_header_ins_co_name'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_header_ins_co_name']);
+  update_post_meta($post_id, '_gda_meta_key_header_ins_co_name', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_ins_co_name_id'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_ins_co_name_id']);
+  update_post_meta($post_id, '_gda_meta_key_ins_co_name_id', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_header_ins_co_policy_number'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_header_ins_co_policy_number']);
+  update_post_meta($post_id, '_gda_meta_key_header_ins_co_policy_number', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_ins_co_policy_number_id'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_ins_co_policy_number_id']);
+  update_post_meta($post_id, '_gda_meta_key_ins_co_policy_number_id', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_header_what_float_doing'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_header_what_float_doing']);
+  update_post_meta($post_id, '_gda_meta_key_header_what_float_doing', $gda_data);
+}
+
+if (isset($_POST['gda_meta_field_what_float_doing_id'])) {
+  $gda_data = sanitize_text_field($_POST['gda_meta_field_what_float_doing_id']);
+  update_post_meta($post_id, '_gda_meta_key_what_float_doing_id', $gda_data);
+}
+
